9c22fea83af136d863ce18ea647190d35a6eb3e3
Justinus Kevin <justinuskevin@gmail.com>
2017-07-29 21:40:31 -0500

    Added comments

--- app/src/main/java/com/example/kevinjustinus/opencv/MainActivity.java
+++ app/src/main/java/com/example/kevinjustinus/opencv/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private boolean permissionGranted = false;
 
+    // check if OpenCV successfully loaded into project
     static {
         if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "OpenCV not loaded");
@@ -41,30 +42,38 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     int iHighS = 255;
     int iLowV = 10;
     int iHighV = 255;
-    Mat imgHSV, imgThresholded;
+
+    Mat imgHSV, imgThresholded; // image matrix input, output
     Scalar sc1, sc2;
     JavaCameraView cameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // set activity layout to landscape in fullscreen
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        // check if permission to camera has been granted
         if (!permissionGranted) {
             checkPermissions();
         }
 
+        // lower and upper limits to HSV filter
         sc1 = new Scalar(iLowH, iLowS, iLowV);
         sc2 = new Scalar(iHighH, iHighS, iHighV);
+
+        // not needed in our app, just in this demo
         cameraView = (JavaCameraView) findViewById(R.id.cameraview);
         cameraView.setCameraIndex(0); // 0 for rear and 1 for front
         cameraView.setCvCameraViewListener(this);
         cameraView.enableView();
     }
 
+    // don't worry about this
     @Override
     protected void onPause() {
         super.onPause();
@@ -77,18 +86,23 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         imgThresholded = new Mat(width, height, CvType.CV_16UC4);
     }
 
+    // don't worry about this
     @Override
     public void onCameraViewStopped() {
 
     }
 
+    // IMPORTANT PIECE HERE!!!
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        // convert image type of source into a matrix (BGR to HSV)
         Imgproc.cvtColor(inputFrame.rgba(), imgHSV, Imgproc.COLOR_BGR2HSV);
+        // return indices in source image which satisfies the lower & upper boundaries
         Core.inRange(imgHSV, sc1, sc2, imgThresholded);
         return imgThresholded;
     }
 
+    // don't worry about this
     private boolean checkPermissions() {
 
         int permissionCheck = ContextCompat.checkSelfPermission(this,
@@ -102,7 +116,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         } else {
             return true;
         }
-
 
     }
 
